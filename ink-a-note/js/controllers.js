7d6b6ab8088d724dc224dd90170ad191c91d7d49
Dave Falke <dmfalke@gmail.com>
2013-05-26 10:44:09 -0400

    added ink-a-note stuff

---
+++ ink-a-note/js/controllers.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function NoteCtrl($scope) {
+  $scope.font = "condiment";
+
+  $scope.message = "Dear Mr./Ms. Last Name:\n\n" +
+
+    "It was very enjoyable to speak with you today about the assistant account executive position at the Smith Agency. The job seems to be an excellent match for my skills and interests. The creative approach to account management that you described confirmed my desire to work with you.\n\n" +
+
+    "In addition to my enthusiasm, I will bring to the position strong writing skills, assertiveness, and the ability to encourage others to work cooperatively with the department.\n\n" +
+
+    "I appreciate the time you took to interview me. I am very interested in working for you and look forward to hearing from you regarding this position.\n\n" +
+
+    "Sincerely,\n\n" +
+
+    "Your Name\n" +
+    "Email Address\n" +
+    "Address\n" +
+    "Phone Number";
+}
